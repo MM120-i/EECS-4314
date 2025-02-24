1ceaf753d9e6f817a40af2d44f907df2bb35ad9b
christie-t <christietang23@gmail.com>
2025-02-23 19:56:23 -0500

    added components to dashboard

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,25 +1,29 @@
 import { poppins } from "../componets/fonts";
+import WeeklySpend from "./WeeklySpend";
+import SavingsAccount from "./SavingsAccount";
+
 
 export default function Page() {
-    return(
+    return (
         <main>
-        <h1 className={`${poppins.className} mb-4 text-xl md:text-2xl`}>
-            Dashboard
-        </h1>
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-           {/* <Suspense fallback={<CardsSkeleton/>}>
-                <CardWrapper/>
-           </Suspense> */}
-        </div>
-        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-            {/* <Suspense fallback={<RevenueChartSkeleton/>}>
-                <RevenueChart />
-            </Suspense>
-            <Suspense fallback={<LatestInvoicesSkeleton/>}>
-                <LatestInvoices/>  
-            </Suspense> */}
-        </div>
-    </main>
-    )
-    
+            <h1 className={`${poppins.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+               {/* <Suspense fallback={<CardsSkeleton/>}>
+                    <CardWrapper/>
+               </Suspense> */}
+            </div>
+
+            {/* Apply flex-col to stack items vertically */}
+            <div className="p-10 bg-gray-100 min-h-screen flex flex-col gap-6">
+                <div className="w-full max-w-4xl h-[290px] bg-gray shadow-md rounded-lg">
+                    <WeeklySpend />
+                </div>
+                <div>
+                    <SavingsAccount />
+                </div>
+            </div>
+        </main>
+    );
 }
