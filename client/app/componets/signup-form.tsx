5845a36642302a284965526fcc18fcb0cf9ad941
Ayub Osman <obuya@Obu-desk>
2025-03-09 14:05:51 -0400

    watch it work

--- client/app/componets/signup-form.tsx
+++ client/app/componets/signup-form.tsx
@@ -5,26 +5,46 @@ import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
+  PencilIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useSearchParams } from 'next/navigation';
 import { useActionState } from 'react';
-import { authenticate } from '../lib/actions';
+import { register } from '../lib/actions';
 import Link from 'next/link';
 
-export default function LoginForm() {
+export default function RegisterForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
-  const [errorMessage, formAction, isPending] = useActionState(authenticate,undefined);
+  const [errorMessage, formAction, isPending] = useActionState(register,undefined);
   return (
     <>
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-0 pt-8">
         <h1 className={`${poppins.className} mb-3 text-2xl`}>
-          Please log in!
+          Create Your account!
         </h1>
         <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Enter your name"
+                required
+              />
+              <PencilIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
           <div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
@@ -67,7 +87,7 @@ export default function LoginForm() {
         </div>
         <input type="hidden" name="redirectTo" value={callbackUrl}/>
         <Button className="mt-4 w-full" aria-disabled={isPending}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          Sign Up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
           {errorMessage && (
@@ -80,7 +100,7 @@ export default function LoginForm() {
       </div>
     </form>
     <div className="flex justify-center text-sm text-black">
-          <span>Don't have an account?<Link href="/register" className="ml-1.5 text-blue">Sign Up</Link></span>
+          <span>Have an account?<Link href="/login" className="ml-1.5 text-blue">Log In</Link></span>
         </div>
     </>
   );
