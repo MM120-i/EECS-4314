39b931b9cdc3f1a0b1d6803a9bb7bf9959e208fc
Mahim <mahimmarufuzzaman1234@gmail.com>
2025-03-14 16:03:35 -0400

    User Schema Improved

--- server/models/User.js
+++ server/models/User.js
@@ -2,9 +2,26 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "User Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please enter a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "User Password is required"],
+      minLength: 6,
+    },
     transactions: [
       { type: mongoose.Schema.Types.ObjectId, ref: "Transaction" },
     ],
