e0d6bf2c27bbe46ea65c67eb24d0797e3d0a3e84
Mahim <mahimmarufuzzaman1234@gmail.com>
2025-04-05 17:25:32 -0400

    updated config file

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
   output: "export",
+  basePath: "/EECS-4314",
+  assetPrefix: "/EECS-4314/",
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "5mb",
